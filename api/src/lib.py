6039d656e47dfab56b84da724b29b824df09aebb
tyellison <48420980+tyellison@users.noreply.github.com>
2023-06-22 16:55:40 -0700

    adding distributed search

--- api/src/lib.py
+++ api/src/lib.py
@@ -1,12 +1,67 @@
+import time
+import multiprocessing as mp
 import numpy as np
+from typing import Union
+from hypergraph import HyperGraph
+from models import InputData
 
-val_map = {0: 0, 20: 1, 39: 2, 40: 3, 78: 4, 100: 5, 195: 6}
-
-
-def preprocess(fname="prefs.csv"):
+def preprocess(fname: str = "prefs.csv") -> np.ndarray:
+    val_map = {0: 0, 20: 1, 39: 2, 40: 3, 78: 4, 100: 5, 195: 6}
     prefs = np.loadtxt(fname, delimiter=",", dtype=np.uint64)
     rows, cols = prefs.shape
     for i in range(rows):
         for j in range(cols):
             prefs[i, j] = val_map[prefs[i, j]]
     return prefs
+
+
+def search(hg: HyperGraph) -> Union[HyperGraph, None]:
+    hg.solve()
+    if hg.is_valid_schedule():
+        return hg
+    return None
+
+
+def distributed_search(req_body: InputData = None) -> Union[HyperGraph, None]:
+    courses = 33
+    times = 51
+    teachers = 29
+
+    dims = {"courses": courses, "times": times, "teachers": teachers}
+
+    prefs = np.random.randint(7, size=(teachers, times, courses), dtype=np.uint64)
+    #prefs = np.loadtxt("formatted_prefs.csv", delimiter=",")
+    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
+    max_iter = 2500
+    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
+    p_tgt = 3
+    num_workers = 8
+    
+    try:
+        mp.set_start_method("spawn", force=True)
+    
+    except RuntimeError:
+        pass
+
+    with mp.get_context("spawn").Pool() as mp_pool:
+        hg_type = type(HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)) 
+        ret_types = []
+        max_runtime = 60
+        start_time = time.time()
+        while hg_type not in ret_types and (time.time() - start_time) < max_runtime:
+            hg_pool = [
+                HyperGraph(dims, prefs, loads, max_iter, P, p_tgt) 
+                for i in range(num_workers)
+            ]
+            res = mp_pool.map(search, hg_pool)
+            ret_types = [type(graph) for graph in res]
+    
+    valid_schedules = [
+        schd for schd in res if isinstance(schd, hg_type)
+    ] 
+    
+    if len(valid_schedules) > 0:
+        valid_schedules.sort(key = lambda hg: hg.calc_reward())
+        return valid_schedules[0]
+    
+    return None
--- api/src/main.py
+++ api/src/main.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
-
-from typing import Union
-import numpy as np
-from fastapi import FastAPI
 from hypergraph import HyperGraph
-from lib import preprocess
+import numpy as np
+from lib import distributed_search
+from fastapi import FastAPI
+from typing import Union
 from models import Success, Error, InputData, IsValidSchedule, Schedule
-from ray import serve
 
 
 app = FastAPI(
@@ -27,7 +25,7 @@ app = FastAPI(
     ],
 )
 
-'''
+
 @app.post(
     "/schedule/create",
     response_model=Schedule,
@@ -38,30 +36,21 @@ def create_schedule(body: InputData = None) -> Union[Schedule, Error]:
     """
     Algorithm 1 endpoint to generate a schedule
     """
-    courses = 33
-    times = 51
-    teachers = 29
-
-    dims = {"courses": courses, "times": times, "teachers": teachers}
-
-    # prefs = np.random.randint(7, size=(teachers, times, courses), dtype=np.uint64)
-    prefs = np.loadtxt("formatted_prefs.csv", delimiter=",")
-    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-    max_iter = 2500
-    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-    p_tgt = 3
-
-    hg = HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)
-    hg.solve()
-    
-    assignments = []
-    for key, _ in hg.sparse().items():
-        assignments.append([num.item() for num in key])
+    result = distributed_search(body)
+    if result == None:
+        return Schedule(
+            assignments = [],
+            valid = False,
+            complete = False
+        )
+    fastapi_type_compatible_assignments = []
+    for key, _ in result.sparse().items():
+        fastapi_type_compatible_assignments.append([num.item() for num in key])
         
-    return {
-        "assignments": list(assignments),
-        "valid" : hg.is_valid_schedule()
-    }
+    return Schedule(
+        assignments = fastapi_type_compatible_assignments, 
+        valid = result.is_valid_schedule()
+    )
 
 @app.post(
     "/schedule/validate",
@@ -89,70 +78,6 @@ def validate_schedule(body: Schedule = None) -> Union[IsValidSchedule, Error]:
     hg = HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)
     hg.solve()
 
-    return {"valid": hg.is_valid_schedule()}
-'''
-
-#'''
-@serve.deployment(route_prefix="/schedule")
-@serve.ingress(app)
-class Algorithm1:
-    @app.post(
-        "/create",
-        response_model=Schedule,
-        responses={"400": {"model": Error}},
-        tags=["algorithm1"],
+    return IsValidSchedule(
+            valid = hg.is_valid_schedule()
     )
-    def create_schedule(body: InputData = None) -> Union[Schedule, Error]:
-        """
-        Algorithm 1 endpoint to generate a schedule
-        """
-        courses = 33
-        times = 51
-        teachers = 29
-
-        dims = {"courses": courses, "times": times, "teachers": teachers}
-
-        prefs = np.loadtxt("formatted_prefs.csv", delimiter=",")
-        loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-        max_iter = 2500
-        P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-        p_tgt = 3
-
-        assignments = []
-        for key, _ in hg.sparse().items():
-            assignments.append([num.item() for num in key])
-        
-        return {
-            "assignments": list(assignments),
-            "valid" : hg.is_valid_schedule()
-        }
-
-    @app.post(
-        "/validate",
-        response_model=IsValidSchedule,
-        responses={"400": {"model": Error}},
-        tags=["algorithm1"],
-    )
-    def validate_schedule(body: Schedule = None) -> Union[IsValidSchedule, Error]:
-        """
-        Algorithm 1 endpoint to validate an existing schedule
-        """
-        courses = 33
-        times = 51
-        teachers = 29
-
-        dims = {"courses": courses, "times": times, "teachers": teachers}
-
-        prefs = np.random.randint(7, size=(teachers, times, courses), dtype=np.uint64)
-        loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-        max_iter = 2500
-        P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-        p_tgt = 3
-
-        hg = HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)
-        hg.solve()
-
-        return {"valid": hg.is_valid_schedule()}
-
-deployment_graph = Algorithm1.bind()
-#'''
