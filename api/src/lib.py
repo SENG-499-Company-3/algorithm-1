af4101b9e3891a2feb5d9332d76a823786becdf7
tyellison <tyellison05@hotmail.com>
2023-06-24 07:42:34 -0700

    refactoring type conversion code to be a helper function and moving mock input data to program scope to make driver search functions more readable

--- api/src/lib.py
+++ api/src/lib.py
@@ -5,34 +5,49 @@ from typing import Union, List
 from hypergraph import HyperGraph
 from models import InputData
 
+
+TIMEOUT = 60
+courses = 33
+times = 51
+teachers = 29
+dims = {"courses": courses, "times": times, "teachers": teachers}
+#prefs = np.loadtxt("formatted_prefs.csv", delimiter=",")
+prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint64)
+loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
+max_iter = 1500
+P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
+p_tgt = 4
+
+
+
 def preprocess(fname: str = "prefs.csv") -> np.ndarray:
     val_map = {0: 0, 20: 1, 39: 2, 40: 3, 78: 4, 100: 5, 195: 6}
     prefs = np.loadtxt(fname, delimiter=",", dtype=np.uint64)
     rows, cols = prefs.shape
+    
     for i in range(rows):
         for j in range(cols):
             prefs[i, j] = val_map[prefs[i, j]]
+    
     return prefs
 
+
+def numpy_to_fastapi_type_conversion(hg: HyperGraph) -> List[List[int]]:
+    items = hg.sparse().items()
+
+    fastapi_type_compatible_assignments = [
+        [num.item() for num in key]
+        for key, _ in items
+    ]
+    
+    return fastapi_type_compatible_assignments 
+
+
 def sequential_search(input_data: InputData = None) -> Union[HyperGraph, None]:
-    courses = 33
-    times = 51
-    teachers = 29
-
-    dims = {"courses": courses, "times": times, "teachers": teachers}
-
-    #prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint64)
-    prefs = np.loadtxt("formatted_prefs.csv", delimiter=",")
-    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-    max_iter = 1000
-    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-    p_tgt = 3
-
-    timeout = 60
-
     hg = HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)
     start_time = time.time() 
-    while (start_time - time.time()) < timeout:
+    
+    while (start_time - time.time()) < TIMEOUT:
         hg.solve()
         if hg.is_valid_schedule():
             return hg
@@ -41,23 +56,10 @@ def sequential_search(input_data: InputData = None) -> Union[HyperGraph, None]:
 
 
 def batch_search(input_data: InputData = None) -> Union[HyperGraph, None]:
-    courses = 33
-    times = 51
-    teachers = 29
-
-    dims = {"courses": courses, "times": times, "teachers": teachers}
-
-    prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint64)
-    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-    max_iter = 2500
-    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-    p_tgt = 4
-
-    timeout = 60
     batch_size = 5 
-
     start_time = time.time()
-    while (start_time - time.time()) < timeout:
+    
+    while (start_time - time.time()) < TIMEOUT:
         hypergraphs = [HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)] * batch_size
 
         for hg in hypergraphs:
@@ -84,18 +86,8 @@ def async_solve(hypergraphs: List[HyperGraph]) -> Union[HyperGraph, None]:
         
     return None
 
+
 def distributed_sequential_search(input_data: InputData = None) -> Union[HyperGraph, None]:
-    courses = 33
-    times = 51
-    teachers = 29
-
-    dims = {"courses": courses, "times": times, "teachers": teachers}
-
-    prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint64)
-    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-    max_iter = 2500
-    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-    p_tgt = 3
     num_workers = 4
     batch_size = 5
     
@@ -108,9 +100,9 @@ def distributed_sequential_search(input_data: InputData = None) -> Union[HyperGr
     with mp.get_context("spawn").Pool() as mp_pool:
         hg_type = type(HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)) 
         ret_types = []
-        max_runtime = 60
         start_time = time.time()
-        while hg_type not in ret_types and (time.time() - start_time) < max_runtime:
+        
+        while hg_type not in ret_types and (time.time() - start_time) < TIMEOUT:
             hg_pool = [
                 [HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)]
                 for i in range(num_workers)
@@ -130,17 +122,6 @@ def distributed_sequential_search(input_data: InputData = None) -> Union[HyperGr
 
 
 def distributed_batch_search(input_data: InputData = None) -> Union[HyperGraph, None]:
-    courses = 33
-    times = 51
-    teachers = 29
-
-    dims = {"courses": courses, "times": times, "teachers": teachers}
-
-    prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint64)
-    loads = np.array([3 for i in range(teachers)], dtype=np.uint64)
-    max_iter = 2500
-    P = np.array([0, 1, 2, 3, 4, 5, 6], dtype=np.uint64)
-    p_tgt = 3
     num_workers = 4
     batch_size = 5
     
@@ -153,9 +134,9 @@ def distributed_batch_search(input_data: InputData = None) -> Union[HyperGraph, 
     with mp.get_context("spawn").Pool() as mp_pool:
         hg_type = type(HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)) 
         ret_types = []
-        max_runtime = 60
         start_time = time.time()
-        while hg_type not in ret_types and (time.time() - start_time) < max_runtime:
+        
+        while hg_type not in ret_types and (time.time() - start_time) < TIMEOUT:
             hg_pool = [
                 [HyperGraph(dims, prefs, loads, max_iter, P, p_tgt)] * batch_size 
                 for i in range(num_workers)
