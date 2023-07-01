14050af0fca29e1fd336320bba995ede61e5b623
tyellison <tyellison05@hotmail.com>
2023-07-01 13:27:40 -0700

    removing test script

--- api/src/test.py
+++
@@ -1,5 +0,0 @@
-from search_drivers import batch_driver, sequential_driver
-
-
-hg = sequential_driver()
-print(f"valid: {hg.is_valid_schedule()}\nsufficient reward: {hg.sufficient_reward}\nreward: {hg.calc_reward()}\nassignments: {hg.sparse()}")
