4a264de041a4e5cc4ef8a2e942c6a62afaf36c91
tyellison <tyellison05@hotmail.com>
2023-07-02 13:45:23 -0700

    rebasing on dev branch

--- api/src/test.py
+++
@@ -1,8 +0,0 @@
-from search_drivers import distributed_driver, sequential_driver
-import time
-
-start = time.time()
-hg = distributed_driver()
-end = time.time()
-print(f"run-time: {end - start}\nvalid: {hg.is_valid_schedule()}\niter: {hg.iter}\nsufficient reward: {hg.sufficient_reward}\nreward: {hg.calc_reward()}\n\nassignments:\n{hg.sparse_tensor}\n\npreferences:\n{hg.prefs}")
-#hg.plot()
