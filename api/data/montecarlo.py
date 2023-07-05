990df508f479945018737362de9e15811477a61d
Seiya Terada <seiyaterada123@gmail.com>
2023-07-05 13:19:26 -0700

    clean up code a bit

--- api/data/montecarlo.py
+++ api/data/montecarlo.py
@@ -11,29 +11,36 @@ def generate_teacher_loads(teachers):
         generated_loads.append(np.random.randint(1, 4))
     return generated_loads
 
-all_prefs = []
-all_loads = []
-num_simulations = 5
+def main():
+    all_prefs = []
+    all_loads = []
+    num_simulations = 5
+    num_teachers = 29
 
-P = np.arange(0, 7)
-prefs = np.loadtxt('./formatted_prefs.csv', delimiter=',')
-# print(generate_teacher_loads(29))
-p_counts, bins = np.histogram(prefs, bins=np.arange(0,8))
-pref_probs = p_counts/p_counts.sum()
+    # Print generated teach lods
+    # print(generate_teacher_loads(num_teachers))
 
-for i in range(num_simulations):
-    all_prefs.append(generate_prefs(P, pref_probs))
-    all_loads.append(generate_teacher_loads(29))
-array = np.asarray(all_prefs)
+    P = np.arange(0, 7)
+    prefs = np.loadtxt('./formatted_prefs.csv', delimiter=',')
+    p_counts, bins = np.histogram(prefs, bins=np.arange(0,8))
+    pref_probs = p_counts/p_counts.sum()
 
-# Plot last generated preferences
-plt.hist(all_prefs[-1][-1], bins)
-plt.show()
+    for i in range(num_simulations):
+        all_prefs.append(generate_prefs(P, pref_probs))
+        all_loads.append(generate_teacher_loads(29))
+    array = np.asarray(all_prefs)
 
-# Save generated preferences to csv
-# Each case is a 29x33 matrix divided by a newline with "Case i" as the header
-with open('./generated_prefs.csv', 'w') as f:
-    for i in range(array.shape[0]):
-        f.write("Case " + str(i+1) + "\n")
-        np.savetxt(f, array[i], delimiter=',', fmt='%d')
-f.close()
+    # Plot last generated preferences
+    plt.hist(all_prefs[-1][-1], bins)
+    plt.show()
+
+    # Save generated preferences to csv
+    # Each case is a 29x33 matrix divided by a newline with "Case i" as the header
+    with open('./generated_prefs.csv', 'w') as f:
+        for i in range(array.shape[0]):
+            f.write("Case " + str(i+1) + "\n")
+            np.savetxt(f, array[i], delimiter=',', fmt='%d')
+    f.close()
+
+if __name__ == '__main__':
+    main()
