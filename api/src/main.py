ffc2aaf3df2bb6588ed8671e3338b972d3787cc3
tyellison <48420980+tyellison@users.noreply.github.com>
2023-07-27 18:07:58 -0700

    adding invalid mock object to validate endpoint to test false scenario with empty request

--- api/src/main.py
+++ api/src/main.py
@@ -7,7 +7,7 @@ from drivers import validate_driver
 from parse import parse
 from generate_schedule import generate_schedule
 from mock_input_data import MOCK_INPUT_DATA
-from mock_schedule import MOCK_VALID_SCHEDULE
+from mock_schedule import MOCK_VALID_SCHEDULE, MOCK_INVALID_PROFESSOR_SCHEDULE
 
 
 app = FastAPI(
@@ -70,7 +70,7 @@ def create_schedule(input_data: InputData = MOCK_INPUT_DATA) -> Union[Schedule, 
     responses={"400": {"model": Error}},
     tags=["algorithm1"],
 )
-def validate_schedule(schedule: Schedule = MOCK_VALID_SCHEDULE) -> Union[IsValidSchedule, Error]:
+def validate_schedule(schedule: Schedule = MOCK_INVALID_PROFESSOR_SCHEDULE) -> Union[IsValidSchedule, Error]:
     """
     Algorithm 1 endpoint to validate an existing schedule
     """
